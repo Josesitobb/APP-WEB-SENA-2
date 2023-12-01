6d2459da5cd3554901b4343822d966c3833f8c5c
Josesitobb <124468479+Josesitobb@users.noreply.github.com>
2023-12-01 16:32:47 -0500

    octavo1.2 intento

--- modules/admin/theme/db.php
+++ modules/admin/theme/db.php
@@ -1,37 +1,29 @@
 <?php
-
-
 $servername = "josecitas.mysql.database.azure.com";
 $username = "JOSE";
 $password = "Celular528";
 $db = "mydb";
 $port = 3306;
 
-
 $ssl_cert = realpath('../../../base de datos/DigiCertGlobalRootCA.crt.pem');
-
-
-
 
 // Configuración de la conexión
 $conn = mysqli_init();
-mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
 
 // Establecer la ruta del certificado
 mysqli_ssl_set($conn, NULL, NULL, $ssl_cert, NULL, NULL);
 
-// Conectar a la base de datos
-mysqli_real_connect($conn, $servername, $username, $password, $db, $port);
+// Configuración de opciones SSL
+mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
 
-// Probar la conexión
-if($conn){
-    echo "hay conexion";
-}else{
-    echo "no hay conexion";
+// Intentar conectar
+if (mysqli_real_connect($conn, $servername, $username, $password, $db, $port)) {
+    echo "Hay conexión";
+} else {
+    // Manejar el error en caso de que la conexión falle
+    echo "No hay conexión. Error: " . mysqli_connect_error();
 }
-// Probar la conexion
-// if(!$conn){
-//     die("Muy bot".mysqli_connect_error());
-// }
-// echo "La conexion esta muy fina"
-// ?>
+
+// Cerrar la conexión
+mysqli_close($conn);
+?>
