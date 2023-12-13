22461bfac827e13a2c62ef1bd25f12574d28c29b
Josesitobb <124468479+Josesitobb@users.noreply.github.com>
2023-12-13 10:26:22 -0500

    db

--- modules/admin/theme/db.php
+++ modules/admin/theme/db.php
@@ -1,28 +1,56 @@
 <?php
-$servername = "josecitas.mysql.database.azure.com";
-$username = "JOSE";
-$password = "Celular528";
-$db = "mydb";
-$port = 3306;
 
-$ssl_cert = realpath('../../../base de datos/DigiCertGlobalRootCA.crt.pem');
+// // conexion azure
+// // $servername = "josecitas.mysql.database.azure.com";
+// // $username = "JOSE";
+// // $password = "Celular528";
+// // $db = "mydb";
+// // $port = 3306;
 
-// Configuración de la conexión
-$conn = mysqli_init();
+// $servername = "locahost";
+// $username = "root";
+// $password = "Celular528";
+// $db = "mydb";
+// $port = 3306;
 
-// Establecer la ruta del certificado
-mysqli_ssl_set($conn, NULL, NULL, $ssl_cert, NULL, NULL);
 
-// Configuración de opciones SSL
-mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+// $ssl_cert = realpath('../../../base de datos/DigiCertGlobalRootCA.crt.pem');
 
-// Intentar conectar
-// if (mysqli_real_connect($conn, $servername, $username, $password, $db, $port)) {
-//     echo "Hay conexión";
-// } else {
-//     // Manejar el error en caso de que la conexión falle
-//     echo "No hay conexión. Error: " . mysqli_connect_error();
+// // Configuración de la conexión
+// $conn = mysqli_init();
+
+// // Establecer la ruta del certificado
+// mysqli_ssl_set($conn, NULL, NULL, $ssl_cert, NULL, NULL);
+
+// // Configuración de opciones SSL
+// mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+
+// // Intentar conectar
+// // if (mysqli_real_connect($conn, $servername, $username, $password, $db, $port)) {
+// //     echo "Hay conexión";
+// // } else {
+// //     // Manejar el error en caso de que la conexión falle
+// //     echo "No hay conexión. Error: " . mysqli_connect_error();
+// // }
+
+
+// conexion local
+
+$servername="localhost";
+$username ="root";
+$password ="";
+$db="mydb";
+
+$conn=mysqli_connect($servername,$username,$password,$db);
+
+// Probar la conexion
+// if(!$conn){
+//     die("Muy bot".mysqli_connect_error());
 // }
+// echo "La conexion esta muy fina"
+// 
+
+
 
 
 ?>
