ed8122f624c34bb1b56808e5325c5a99865986f7
Josesitobb <joseguerra.jg683@gmail.com>
2024-03-09 21:21:30 -0500

    SE AGREGA CONEXION DE AZURE

--- modules/admin/actualizar_cita.php
+++ modules/admin/actualizar_cita.php
@@ -1,5 +1,7 @@
 <?php
 // actualizar_cita.php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 include("./db.php");
 
--- modules/admin/obtener_precio_servicio.php
+++ modules/admin/obtener_precio_servicio.php
@@ -1,5 +1,7 @@
 <?php
 // obtener_precio_servicio.php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 include("./db.php");
 
