367af3bdb9367b6b340e2ddabbd9ef94646f0b00
Josesitobb <joseguerra.jg683@gmail.com>
2024-04-03 18:56:11 -0500

    A5

--- controllers/db.php
+++ controllers/db.php
@@ -1,20 +1,20 @@
 <?php
 // db.php
 
-// CONEXION A LA BASE DE DATOS
-$servername="localhost";
-$username ="root";
-$password ="";
-$db="SGCitas";
+// // CONEXION A LA BASE DE DATOS
+// $servername="localhost";
+// $username ="root";
+// $password ="";
+// $db="SGCitas";
 
-$conn=mysqli_connect($servername,$username,$password,$db);
+// $conn=mysqli_connect($servername,$username,$password,$db);
 
-// Verificar la conexión
-if (!$conn) {
-    die("Conexión fallida: " . mysqli_connect_error());
-}else{
-    echo "existe";
-}
+// // Verificar la conexión
+// if (!$conn) {
+//     die("Conexión fallida: " . mysqli_connect_error());
+// }else{
+//     echo "existe";
+// }
 
 
 // // CONEXION A LA BASE DE DATOS
